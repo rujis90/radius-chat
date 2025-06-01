259398d799725adac21e3a8889bbed6347bf7c31
henricat <henri.rujala@outlook.com>
2025-06-01 11:25:41 +0300

    latest

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def ws_route(ws):
         broadcast_peer_list()
         clean_rooms()
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
